41609ded4ad1fefbadea95233cc7e8b7de559ab1
Shavian-info <contact@shavian.info>
2024-01-12 17:09:18 +1100

    Added VY and vy ligatures as requested in https://github.com/Shavian-info/interalia/issues/6#issue-1999781068 and lowercase Latin iota.

--- make_interalia.sh
+++ make_interalia.sh
@@ -4,25 +4,6 @@ fontmake "$PWD/src/InterAlia.glyphspackage" -o ttf otf -i -f
 
 fontmake "$PWD/src/InterAlia.glyphspackage" -o variable -f
 
-#woff2_compress $PWD/instance_ttf/InterAlia-Black.ttf > $PWD/instance_woff2/InterAlia-Black.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-BlackItalic.ttf > $PWD/instance_woff2/InterAlia-BlackItalic.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-Bold.ttf > $PWD/instance_woff2/InterAlia-Bold.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-BoldItalic.ttf > $PWD/instance_woff2/InterAlia-BoldItalic.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-ExtraBold.ttf > $PWD/instance_woff2/InterAlia-ExtraBold.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-ExtraBoldItalic.ttf > $PWD/instance_woff2/InterAlia-ExtraBoldItalic.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-ExtraLight.ttf > $PWD/instance_woff2/InterAlia-ExtraLight.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-ExtraLightItalic.ttf > $PWD/instance_woff2/InterAlia-ExtraLightItalic.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-Italic.ttf > $PWD/instance_woff2/InterAlia-Italic.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-Light.ttf > $PWD/instance_woff2/InterAlia-Light.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-LightItalic.ttf > $PWD/instance_woff2/InterAlia-LightItalic.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-Medium.ttf > $PWD/instance_woff2/InterAlia-Medium.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-MediumItalic.ttf > $PWD/instance_woff2/InterAlia-MediumItalic.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-Regular.ttf > $PWD/instance_woff2/InterAlia-Regular.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-SemiBold.ttf > $PWD/instance_woff2/InterAlia-SemiBold.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-SemiBoldItalic.ttf > $PWD/instance_woff2/InterAlia-SemiBoldItalic.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-Thin.ttf > $PWD/instance_woff2/InterAlia-Thin.woff2
-#woff2_compress $PWD/instance_ttf/InterAlia-ThinItalic.ttf > $PWD/instance_woff2/InterAlia-ThinItalic.woff2
-
 font_files=(
     "InterAlia-Black.ttf"
     "InterAlia-BlackItalic.ttf"
